df9724039a84a3845627aa80af54cc518065e12d
daniel522 <Seasailor@mail.com>
2024-05-06 17:21:19 -0400

    Update vast8.sh

--- vast8.sh
+++ vast8.sh
@@ -28,7 +28,7 @@ sudo chmod +x build.sh > /dev/null 2>&1
 sudo ./build.sh > /dev/null 2>&1
 echo "STEP 7 of 10: Permissions set!"
 
-sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xf3fCA2BD522603043023A7f13c5B525802C9006D/g' config.conf > /dev/null 2>&1
+sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x980d3718d04A23750E3a797b4EB98Bd59fb769Bd/g' config.conf > /dev/null 2>&1
 echo "STEP 8 of 10: Replaced ETH address"
 
 sudo pip install -U -r requirements.txt > /dev/null 2>&1
